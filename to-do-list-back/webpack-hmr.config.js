e95d74dfb64c330a4b1ffccc8ff26a6e75aca58a
Dmitry Lyapin <lyapindm@yandex.ru>
2023-12-22 15:25:27 +0300

    Добавил hot reload для работы с контейнером

---
+++ to-do-list-back/webpack-hmr.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    watchOptions: {
+      // без этого не успевает в Docker Hot Module Reload!!!
+      aggregateTimeout: 100,
+      poll: 100,
+    },
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/, /node_modules/],
+      }),
+      new RunScriptWebpackPlugin({
+        name: options.output.filename,
+        autoRestart: true,
+      }),
+    ],
+  };
+};
